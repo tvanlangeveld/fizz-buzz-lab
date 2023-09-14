5ea567161e2acee6d3f152a80713bb28ac792c24
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-14 08:26:12 -0700

    final commit for reals

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,15 @@
 // Write your solution below this line:
 
 for(let i = 1; i <= 50; i++){
-  if(i % 3 === 0){
+  if(i % 3 === 0 && i % 5){
+    console.log('fizzBuzz')
+  }
+  else if(i % 3 === 0){
     console.log('Fizz')
   } else if(i % 5 === 0){
     console.log('buzz')
-  } else{
+  } 
+  else{
     console.log(i)
   }
 }
