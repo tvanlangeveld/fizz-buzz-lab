80de30105014e149665e985251243226e86aefbb
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-14 07:59:06 -0700

    Final commit project complete and code working properly

---
+++ fizzBuzz.js
@@ -0,0 +1,11 @@
+// Write your solution below this line:
+
+for(let i = 0; i <= 50; i++){
+  if(i % 3 === 0){
+    console.log('Fizz')
+  } else if(i % 5 === 0){
+    console.log('buzz')
+  } else{
+    console.log(i)
+  }
+}
